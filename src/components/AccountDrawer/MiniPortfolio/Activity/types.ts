cdad8ccf5d54592453aa7e7e047cb36f8f6063ca
wangdingkun <5446743+wangdingkun@user.noreply.gitee.com>
2023-11-15 09:52:26 +0800

    first commit

---
+++ src/components/AccountDrawer/MiniPortfolio/Activity/types.ts
@@ -0,0 +1,20 @@
+import { ChainId, Currency } from '@uniswap/sdk-core'
+import { AssetActivityPartsFragment, TransactionStatus } from 'graphql/data/__generated__/types-and-hooks'
+
+type Receipt = AssetActivityPartsFragment['transaction']
+
+export type Activity = {
+  hash: string
+  chainId: ChainId
+  status: TransactionStatus
+  timestamp: number
+  title: string
+  descriptor?: string
+  logos?: Array<string | undefined>
+  currencies?: Array<Currency | undefined>
+  otherAccount?: string
+  receipt?: Omit<Receipt, 'nonce'>
+  nonce?: number | null
+}
+
+export type ActivityMap = { [hash: string]: Activity | undefined }
---
+++ src/constants/chains.ts
@@ -0,0 +1,92 @@
+import { ChainId, SUPPORTED_CHAINS, SupportedChainsType } from '@uniswap/sdk-core'
+
+export const UniWalletSupportedChains = [ChainId.MAINNET, ChainId.ARBITRUM_ONE, ChainId.OPTIMISM, ChainId.POLYGON]
+
+export const CHAIN_IDS_TO_NAMES = {
+  [ChainId.MAINNET]: 'mainnet',
+  [ChainId.GOERLI]: 'goerli',
+  [ChainId.SEPOLIA]: 'sepolia',
+  [ChainId.POLYGON]: 'polygon',
+  [ChainId.POLYGON_MUMBAI]: 'polygon_mumbai',
+  [ChainId.CELO]: 'celo',
+  [ChainId.CELO_ALFAJORES]: 'celo_alfajores',
+  [ChainId.ARBITRUM_ONE]: 'arbitrum',
+  [ChainId.ARBITRUM_GOERLI]: 'arbitrum_goerli',
+  [ChainId.OPTIMISM]: 'optimism',
+  [ChainId.OPTIMISM_GOERLI]: 'optimism_goerli',
+  [ChainId.BNB]: 'bnb',
+  [ChainId.AVALANCHE]: 'avalanche',
+} as const
+
+export function isSupportedChain(chainId: number | null | undefined | ChainId): chainId is SupportedChainsType {
+  return !!chainId && SUPPORTED_CHAINS.indexOf(chainId) !== -1
+}
+
+export function asSupportedChain(chainId: number | null | undefined | ChainId): SupportedChainsType | undefined {
+  return isSupportedChain(chainId) ? chainId : undefined
+}
+
+export const SUPPORTED_GAS_ESTIMATE_CHAIN_IDS = [
+  ChainId.MAINNET,
+  ChainId.POLYGON,
+  ChainId.CELO,
+  ChainId.OPTIMISM,
+  ChainId.ARBITRUM_ONE,
+  ChainId.BNB,
+  ChainId.AVALANCHE,
+] as const
+
+/**
+ * Unsupported networks for V2 pool behavior.
+ */
+export const UNSUPPORTED_V2POOL_CHAIN_IDS = [
+  ChainId.POLYGON,
+  ChainId.OPTIMISM,
+  ChainId.ARBITRUM_ONE,
+  ChainId.BNB,
+  ChainId.ARBITRUM_GOERLI,
+  ChainId.AVALANCHE,
+] as const
+
+export const TESTNET_CHAIN_IDS = [
+  ChainId.GOERLI,
+  ChainId.SEPOLIA,
+  ChainId.POLYGON_MUMBAI,
+  ChainId.ARBITRUM_GOERLI,
+  ChainId.OPTIMISM_GOERLI,
+  ChainId.CELO_ALFAJORES,
+] as const
+
+/**
+ * All the chain IDs that are running the Ethereum protocol.
+ */
+export const L1_CHAIN_IDS = [
+  ChainId.MAINNET,
+  ChainId.GOERLI,
+  ChainId.SEPOLIA,
+  ChainId.POLYGON,
+  ChainId.POLYGON_MUMBAI,
+  ChainId.CELO,
+  ChainId.CELO_ALFAJORES,
+  ChainId.BNB,
+  ChainId.AVALANCHE,
+] as const
+
+export type SupportedL1ChainId = (typeof L1_CHAIN_IDS)[number]
+
+/**
+ * Controls some L2 specific behavior, e.g. slippage tolerance, special UI behavior.
+ * The expectation is that all of these networks have immediate transaction confirmation.
+ */
+export const L2_CHAIN_IDS = [
+  ChainId.ARBITRUM_ONE,
+  ChainId.ARBITRUM_GOERLI,
+  ChainId.OPTIMISM,
+  ChainId.OPTIMISM_GOERLI,
+] as const
+
+export type SupportedL2ChainId = (typeof L2_CHAIN_IDS)[number]
+
+export function isPolygonChain(chainId: number): chainId is ChainId.POLYGON | ChainId.POLYGON_MUMBAI {
+  return chainId === ChainId.POLYGON || chainId === ChainId.POLYGON_MUMBAI
+}
